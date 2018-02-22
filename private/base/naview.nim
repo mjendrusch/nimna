ed1a9e9454758a516f234f09dca0b3254261de5c
Michael <Jendrusch@stud.uni-heidelberg.de>
2018-02-22 20:57:22 +0100

    cleanup

--- private/base/naview.nim
+++
@@ -1,9 +0,0 @@
-#
-# nimna - *NA folding for Nim
-#
-# (c) Copyright 2017 Michael Jendrusch. All rights reserved.
-# This library is licensed under the MIT license.
-# For more information see LICENSE.
-
-proc naviewXyCoordinates*(pairTable: ptr cshort; x: ptr cfloat; y: ptr cfloat): cint {.
-    cdecl, importc: "naview_xy_coordinates", dynlib: rnaLib.}
